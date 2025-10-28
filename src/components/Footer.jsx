01c5a9a1a24288aea2eb771a60bd8c53e46af2c7
brijehssingh <brijeshkumar4705r@gmail.com>
2025-10-28 22:48:21 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer footer-center p-6 text-base-content">
       <aside>
-        <p>© {new Date().getFullYear()} Your Name — Built with React + DaisyUI</p>
+        <p>© {new Date().getFullYear()} BRIJESH SINGH — Built with React + DaisyUI</p>
       </aside>
     </footer>
   );
